00614a482765468cbc7258bfd2741b84e0069bc1
Jay <jaykamas@gmail.com>
2020-09-11 10:55:29 -0400

    Fixed pause persistence bug.

--- background.js
+++ background.js
@@ -1,47 +1,53 @@
-let startingTime = "5:00"
-let currentTimer = startingTime
-let minutesRemaining = currentTimer.split(":")[0]
+let startingTime = "5:00";
+let currentTimer = startingTime;
+let minutesRemaining = currentTimer.split(":")[0];
 let secondsRemaining = minutesRemaining * 60;
 let displayTimer;
-chrome.runtime.onMessage.addListener(sendTimerInfo)
+let isRunning = false;
+chrome.runtime.onMessage.addListener(sendTimerInfo);
 
 function sendTimerInfo(request, sender, sendResponse){
     if (request.text === "Need the time"){
-        sendResponse(currentTimer)
+        sendResponse(currentTimer);
     }
     if (request.text === "start the timer"){
         countDown();
-        sendResponse(secondsRemaining)
+        sendResponse(secondsRemaining);
+    }
+    if (request.text === "are you running?"){
+        sendResponse(isRunning);
     }
     if (request.text === "pause the timer"){
+        isRunning = !isRunning
         clearInterval(displayTimer);
-        sendResponse("Timer has paused")
+        console.log(displayTimer)
+        sendResponse("Timer has paused");
     }
     if (request.text === "restart the timer"){
+        isRunning = !isRunning
         clearInterval(displayTimer);
-        reloadVariables(sendResponse)
-
+        reloadVariables(sendResponse);
     }
     if (request.text === "increase the timer") {
-        let startingMinutes = Number(startingTime.split(":")[0])
-        startingMinutes += 1 
-        startingTime = startingMinutes.toString() + ":00"
-        reloadVariables(sendResponse)
+        let startingMinutes = Number(startingTime.split(":")[0]);
+        startingMinutes += 1;
+        startingTime = startingMinutes.toString() + ":00";
+        reloadVariables(sendResponse);
 
     }
     if (request.text === "decrease the timer") {
-        let startingMinutes = Number(startingTime.split(":")[0])
-        startingMinutes -= 1 
-        startingTime = startingMinutes.toString() + ":00"
-        reloadVariables(sendResponse)
+        let startingMinutes = Number(startingTime.split(":")[0]);
+        startingMinutes -= 1;
+        startingTime = startingMinutes.toString() + ":00";
+        reloadVariables(sendResponse);
     }
 }
 
 function reloadVariables(sendResponse) {
-    currentTimer = startingTime
-    minutesRemaining = currentTimer.split(":")[0]
+    currentTimer = startingTime;
+    minutesRemaining = currentTimer.split(":")[0];
     secondsRemaining = minutesRemaining * 60;
-    sendResponse(currentTimer)
+    sendResponse(currentTimer);
 }
 
 function countDown() {
@@ -49,6 +55,7 @@ function countDown() {
 }
 
 const tick = () => {
+    isRunning = !isRunning
     let myAlarm = new Audio('Fishtank_Bubbles-SoundBibleco-amanda-1550139304.mp3')
     let min = Math.floor(secondsRemaining / 60); 
     let sec = secondsRemaining - (min * 60);
@@ -57,9 +64,9 @@ const tick = () => {
     }
     let timer = min.toString() + ":" + sec;
     currentTimer = timer;
-    console.log(currentTimer)
     if (secondsRemaining === 0){
-        myAlarm.play()
+        isRunning = !isRunning
+        myAlarm.play();
         alert("Timer is up!");
         clearInterval(displayTimer);
     }
